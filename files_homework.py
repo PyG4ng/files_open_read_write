56f80fa49216bb4f629c2ac439f2d7fbc60e32e8
Daniel <danielslackpy@gmail.com>
2021-11-17 10:05:24 +0300

    Cleaning code

--- files_homework.py
+++ files_homework.py
@@ -5,86 +5,99 @@ import json
 CUR_DIR = Path(__file__).resolve().parent
 DATA = CUR_DIR / "recipes.txt"
 
-def get_data(file):
-    """Getting data inside a file and spliting data inside the file in lists on empty strings
+
+def get_data(file) -> list:
+    """Gets content from a .txt file. Splits the content on empty strings and return a list
 
     Args:
-        file (.txt): file with .txt extension
+        file: path to a .txt file
 
-    Returns:
-        list: list of data
+    Returns: list of data
     """
-
     with open(file, "r", encoding='utf-8-sig') as f:
         lines = f.read().splitlines()
-        # Adding an empty string at the end of the "lines" list, will be used to wrap everything between the second to last and the last string in a list
+        # Adding an empty string at the end of the "lines" list. Will be used to wrap everything between
+        # the second to last and the last empty string
         lines.append('')
 
-    # Wrap all strings between two empty strings in a list of strings (data_inside), first string doesn't need to be empty
-    data =[]
-    data_inside=[]
+    all_meals = []
+    meal = []
     for line in lines:
         if line != '':
-            data_inside.append(line)
+            meal.append(line)
         else:
-            data.append(data_inside)
-            data_inside = []
+            all_meals.append(meal)
+            meal = []
 
-    return data
+    return all_meals
 
 
-def make_cook_book(data_ : list) -> dict:
-    """Create a dictionnary out of a list. In the dictionnary each key has a list of dictionnary
+def make_cook_book(data_: list) -> dict:
+    """Create a dictionary out of a list. In the dictionary each key has a list of dictionary
 
     Args:
         data_ (list): list containing lists
 
     Returns:
-        dict: Dictionnary made out of a list containing lists
+        dict: Dictionary made out of a list containing lists
     """
-    book_ = {}
+    cook_book = {}
     for i in range(len(data_)):
         ingredients = []
         a, b, *args = data_[i]
         # args should contain strings with two " | " separators inside like "Egg | 1 | pc"
-        for elem in args:
-            massif = elem.split(' | ')
-            ingredients.append(dict(ingredient_name = massif[0], quantity = massif[1], measure = massif[2]))
-        book_[a] = ingredients
-    return book_
-        
-        
-def get_shop_list_by_dishes(dishes : list, person_count : int) -> dict:
-    """Create a dictionnary of needed ingredients to cook dishes for a number of person
+        for arg in args:
+            arg_list = arg.split(' | ')
+            ingredients.append(dict(ingredient_name=arg_list[0], quantity=arg_list[1], measure=arg_list[2]))
+        cook_book[a] = ingredients
+    return cook_book
+
+
+my_cook_book = make_cook_book(get_data(DATA))
+print('Список рецептов:')
+pprint(my_cook_book)
+
+
+# with open('recipes.json', 'w', encoding='utf-8-sig') as cook_book_file:
+#     json.dump(my_cook_book, cook_book_file,
+#               ensure_ascii=False, indent=4)
+
+
+def get_shop_list_by_dishes(dishes: list, person_count: int) -> dict:
+    """Creates a dictionary of needed ingredients to cook dishes for a number of person
 
     Args:
         dishes (list): list of dishes
-        person_count (int): Number of people
+        person_count (int): Number of person to for
 
-    Returns:
-        dict: Dictionnary of ingredients
+    Returns: Dictionary of ingredients
     """
-    cook_book =  make_cook_book(get_data(DATA))
-    ingredients = []
+    cook_book = make_cook_book(get_data(DATA))
+    all_ingredients = []
     for dish in dishes:
-        ingredients.extend(cook_book.get(dish))
-    # pprint(ingredients)
+        all_ingredients.extend(cook_book.get(dish))
+    # pprint(all_ingredients)
 
     ingredients_dict = {}
-    for elem in ingredients:
-        ingredient_name = elem.get('ingredient_name')
+    for ingredient in all_ingredients:
+        ingredient_name = ingredient.get('ingredient_name')
         if ingredient_name not in ingredients_dict:
-            ingredients_dict[ingredient_name] = dict(quantity = int(elem.get('quantity')) * person_count, measure = elem.get('measure'))
+            ingredients_dict[ingredient_name] = dict(quantity=int(ingredient.get('quantity')) * person_count,
+                                                     measure=ingredient.get('measure'))
         else:
             existing_ingredient = ingredients_dict.get(ingredient_name)
-            quantity_to_add = int(existing_ingredient.get('quantity'))
-            ingredients_dict[ingredient_name] = dict(quantity = quantity_to_add + int(elem.get('quantity')) * person_count, measure = elem.get('measure'))
-    
+            quantity_to_add = existing_ingredient.get('quantity')
+            ingredients_dict[ingredient_name] = dict(
+                quantity=quantity_to_add + int(ingredient.get('quantity')) * person_count,
+                measure=ingredient.get('measure'))
+
     return ingredients_dict
 
 
-pprint(get_shop_list_by_dishes(dishes=['Запеченный картофель', 'Омлет'], person_count=2))
+shop_list = get_shop_list_by_dishes(dishes=['Запеченный картофель', 'Омлет'], person_count=2)
+print('\nИнгедиенты:')
+pprint(shop_list)
 
-# with open('Ingredients.json', 'w', encoding='utf-8-sig') as f:
-#     json.dump(get_shop_list_by_dishes(dishes=['Запеченный картофель', 'Омлет'], person_count=2), f, ensure_ascii=False, indent=4)
-
+# with open('shop_list.json', 'w', encoding='utf-8-sig') as shop_list_file:
+#     json.dump(shop_list, shop_list_file,
+#               ensure_ascii=False, indent=4)
--- merging_files.py
+++ merging_files.py
@@ -1,70 +1,52 @@
 from pathlib import Path
 
-
 CUR_DIR = Path(__file__).resolve().parent
-DATA_FOLDER = CUR_DIR / 'sorted'
-FILES = [f for f in DATA_FOLDER.iterdir()]
+DATA_FOLDER = CUR_DIR / 'sorted_files'
+FILES = [file for file in DATA_FOLDER.iterdir() if file.is_file() and file.suffix == '.txt']
 
-def get_file_data(files_path : list) -> list:
-    """Get the name, lenth and content of .txt files
+
+def get_files_data_sorted_by_number_of_lines(files_path: list) -> list:
+    """
+    Gets a list of paths to .txt files and returns a list of dictionaries with the name, the number of lines (length)
+    and the content of .txt files
 
     Args:
-        files_path (list): list of path to the files
+        files_path (list): list of paths to .txt files
 
-    Returns:
-        list: list of dictionnaries
+    Returns: A list of dictionaries
+
     """
-    file_content_dict=[]
-    for elem in files_path:
-        file_ = {}
-        with open(elem, 'r', encoding='utf-8-sig') as f:
+    list_of_file_parameters = []
+    for file in files_path:
+        file_parameters = {}
+        with open(file, 'r', encoding='utf-8-sig') as f:
             content = f.read().splitlines()
-        file_['name'] = elem.name
-        file_['lenth'] = len(content)
-        file_['content'] = content
-        file_content_dict.append(file_)
-    return file_content_dict
+        file_parameters['name'] = file.name
+        file_parameters['length'] = len(content)
+        file_parameters['content'] = content
+        list_of_file_parameters.append(file_parameters)
+    return sorted(list_of_file_parameters, key=lambda file_param: file_param["length"])
 
 
-def sort_file_content_by_lenth(files_path : list) -> list:
-    """Sort a list of files content by lenth (number of line inside the file)
-
-    Args:
-        files_path (list): list of path to the files
-
-    Returns:
-        list: Sorted list of dictionnaries by lenth value
-    """
-    sorted_file_content=[]
-    file_content_dict = get_file_data(files_path)
-    while file_content_dict:
-        lenn = float('inf')
-        for i in range(len(file_content_dict)):
-            if float(file_content_dict[i].get('lenth')) < lenn:
-                lenn = file_content_dict[i].get('lenth')
-                k = i
-        sorted_file_content.append(file_content_dict[k])
-        file_content_dict.remove(file_content_dict[k])
-    return sorted_file_content
-
-
-def create_merged_file(data_folder ,files_path : list, merged_file_name : str):
-    """Create a .txt file
+def create_merged_file(merged_file_folder, files_path: list, merged_file_name: str):
+    """Gets a list of paths to .txt files, merged them and creates a new .txt  file
 
     Args:
-        data_folder (pathlib.WindowsPath) : path to the folder containig the merged file
-        files_path (list): list of path to the files to merge
-        merged_file_name (str): wanted name of the created file
+        merged_file_folder (path) : path to the folder to contain the merged file
+        files_path (list): list of paths to the .txt files to merge
+        merged_file_name (str): given name to the new file
     """
-    sorted_list = sort_file_content_by_lenth(files_path)
-    with open(f'{data_folder}/{merged_file_name}.txt', 'w', encoding='utf-8-sig') as f:
-        for i in range(len(sorted_list)):
-            f.write(sorted_list[i]['name'] + '\n')
-            f.write(str(sorted_list[i]['lenth']) + '\n')
-            for j in range(len(sorted_list[i]['content'])):
-                n = '' if i == len(sorted_list)-1 and j == len(sorted_list[i]['content'])-1 else '\n'
-                f.write(sorted_list[i]['content'][j] + n)
 
-create_merged_file(DATA_FOLDER,FILES,'merged_files')
+    list_of_file_param = get_files_data_sorted_by_number_of_lines(files_path)
+    merged_file_folder.mkdir(exist_ok=True)
+    with open(f'{merged_file_folder}/{merged_file_name}.txt', 'w', encoding='utf-8-sig') as new_file:
+        for i in range(len(list_of_file_param)):
+            new_file.write(list_of_file_param[i]['name'] + '\n')
+            new_file.write(str(list_of_file_param[i]['length']) + '\n')
+            for j in range(len(list_of_file_param[i]['content'])):
+                # To avoid adding an empty line at the end of the file
+                n = '' if i == len(list_of_file_param) - 1 and j == len(list_of_file_param[i]['content']) - 1 else '\n'
+                new_file.write(list_of_file_param[i]['content'][j] + n)
 
 
+create_merged_file(CUR_DIR / 'merged_file', FILES, 'merged_files')
